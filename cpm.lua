515f6a5130626410acf0c80d6cf186a471019c34
NexAdn <adrianschollmeyer@web.de>
2016-08-24 19:32:55 +0200

    Initial commit.

---
+++ cpm.lua
@@ -0,0 +1,32 @@
+-- Copyright (C) 2016 Adrian Schollmeyer
+
+-- cpm.lua is part of CPM.
+--
+--  CPM is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU Affero General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License and the GNU Affero General Public License for more details.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+--  Licenses can be found in the COPYING-Folder.
+
+-- User specific configuration
+local tConfig = {
+
+}
+
+-- Static configuration (DO NOT CHANGE)
+local tStatic = {
+	sVersion = "0.0.1"
+}
+
+local tData = {
+
+}
+
+
