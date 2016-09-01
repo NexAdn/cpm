4eb33a6327b3d50c690119c0a114312308fde375
NexAdn <adrianschollmeyer@web.de>
2016-09-01 15:34:53 +0200

    Update cpm.lua

--- cpm.lua
+++ cpm.lua
@@ -25,7 +25,7 @@ local tConfig = {
 
 -- Static configuration (DO NOT CHANGE)
 local tStatic = {
-    sVersion = "0.0.1",
+    sVersion = "0.1",
     
     sPackagesFile       = "/packages",
     sDependenciesFile   = "/dependencies",
